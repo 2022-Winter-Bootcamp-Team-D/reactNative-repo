04886c298febfd59e007b666462f033384cf8fe4
HAERYN <syt06119@naver.com>
2023-01-11 22:41:36 +0900

    fix: #6 안쓰는 함수 및 import 삭제

--- src/screens/search/SearchResultScreen.tsx
+++ src/screens/search/SearchResultScreen.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import {ScrollView, View, Text, TextInput} from 'react-native';
-import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
+import {ScrollView, View, Text} from 'react-native';
 import CollapesibleView from "../../components/maplist/CollapsibleView";
 import SearchBar from "../../components/search/SearchBar";
 import mapListStyles from "../../styles/MapListStyles";
@@ -8,9 +7,6 @@ import SearchScreenStyles from "../../styles/screens/SearchScreenStyles";
 import SearchStyles from "../../styles/SearchStyles";
 
 function SearchResultScreen() {
-  const onPress = () => {
-    // .. 실행할 함수
-  }
   return (
     <ScrollView style={SearchScreenStyles.container}>
       <Text style={SearchStyles.searchTitle}>
