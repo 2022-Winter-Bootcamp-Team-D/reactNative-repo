9f7f967364a2ef01a9265eae083c47db7aa76a6a
“mineii” <kmaoo0406@naver.com>
2023-01-11 21:40:25 +0900

    feat: #3 InquiryInput 비밀번호 4자리 제한

--- src/components/inquiry/InquiryInput.tsx
+++ src/components/inquiry/InquiryInput.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 import {View, Image, Text} from 'react-native';            // 꼭 3가지만 있어야하나요! 여기에 버튼 추가 가능한가요
 import { TextInput } from "react-native-gesture-handler";  //이건 몽가요
 import InquiryStyles from "../../styles/InquiryStyles";
 
 function InquiryInput() {
+    const [password, setPassword] = useState('');
     return(
         <View>
             <TextInput
@@ -13,6 +14,9 @@ function InquiryInput() {
             <TextInput
                 style={InquiryStyles.RegisterInput}
                 placeholder="비밀번호(4자리)"
+                secureTextEntry //비밀번호 * 표시, 해제하려면 ={false}입력
+                maxLength={4}
+                onChangeText={text => setPassword(text)}
             />
         </View>
     );
